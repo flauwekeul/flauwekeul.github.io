392ca9e1e0fbda8ab72b83528dcdee8dabcd32aa
Abbe Keultjes <flauwekeul@gmail.com>
2024-01-09 22:15:13 +0100

    --wip-- [skip ci]

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        // consider Akkurat: https://www.dafontfree.io/akkurat-font/
+        sans: ['"Source Sans 3Variable"', ...defaultTheme.fontFamily.sans],
+        serif: ['"Source Serif 4Variable"', ...defaultTheme.fontFamily.serif],
+      },
+    },
+  },
+  plugins: [],
 }
