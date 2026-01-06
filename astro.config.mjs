c989aba8cc3078518b5bd7ad35522ec06ec83e97
Abbe Keultjes <flauwekeul@gmail.com>
2026-01-06 22:04:21 +0100

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 // @ts-check
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
@@ -18,5 +19,10 @@ export default defineConfig({
         weights: [400, 700],
       },
     ],
+  },
+
+  vite: {
+    // @ts-expect-error: https://github.com/withastro/astro/issues/14030#issuecomment-3027129338
+    plugins: [tailwindcss()],
   },
 });
