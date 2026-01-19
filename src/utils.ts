ca05661e609f17307675fa1f0d0cb850769c5eb9
Abbe Keultjes <flauwekeul@gmail.com>
2026-01-19 22:46:31 +0100

    remove animations, add more content

--- src/utils.ts
+++ src/utils.ts
@@ -2,3 +2,6 @@ export const shuffleArray = <T>(array: T[]): T[] => array.toSorted(() => Math.ra
 
 const MILLISECONDS_PER_YEAR = 1000 * 60 * 60 * 24 * 365.25;
 export const yearsSince = (date: Date) => Math.floor((new Date().getTime() - date.getTime()) / MILLISECONDS_PER_YEAR);
+
+export const intercalate = <T, S>(array: T[], separator: S): Array<T | S> =>
+  array.flatMap((item, i) => (i < array.length - 1 ? [item, separator] : [item]));
