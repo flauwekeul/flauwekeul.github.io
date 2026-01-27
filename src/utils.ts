2bce9da2775be7ee7466d62565262b299c73929c
Abbe Keultjes <flauwekeul@gmail.com>
2026-01-27 21:42:29 +0100

    update prettier rules and format all

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,11 @@
-export const shuffleArray = <T>(array: T[]): T[] => array.toSorted(() => Math.random() - 0.5);
+export const shuffleArray = <T>(array: T[]): T[] =>
+  array.toSorted(() => Math.random() - 0.5);
 
 const MILLISECONDS_PER_YEAR = 1000 * 60 * 60 * 24 * 365.25;
-export const yearsSince = (date: Date) => Math.floor((new Date().getTime() - date.getTime()) / MILLISECONDS_PER_YEAR);
+export const yearsSince = (date: Date) =>
+  Math.floor((new Date().getTime() - date.getTime()) / MILLISECONDS_PER_YEAR);
 
 export const intercalate = <T, S>(array: T[], separator: S): Array<T | S> =>
-  array.flatMap((item, i) => (i < array.length - 1 ? [item, separator] : [item]));
+  array.flatMap((item, i) =>
+    i < array.length - 1 ? [item, separator] : [item],
+  );
